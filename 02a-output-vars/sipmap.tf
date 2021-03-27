8d641edbc19c38c97d7093912948a2a7d6a63c76
abhishek das <i.abhishek.dass@gmail.com>
2021-03-27 13:39:49 +0000

    zipmap

---
+++ 02a-output-vars/sipmap.tf
@@ -0,0 +1,22 @@
+variable "keys" {
+  type = list
+  default = ["orange", "pinapple", "strawberry"]
+  description = "keys of the map"
+}
+
+variable "values" {
+  type = list
+  default = ["orange", "yellow", "red"]
+  description = "values of the map"
+}
+
+locals {
+  zipmap = zipmap(var.keys, var.values)
+}
+
+output "zipmap" {
+  value = {
+    direct = zipmap(var.keys, var.values),
+    local = local.zipmap["strawberry"]
+  }
+}
---
+++ docs/19-tf-zipmap.md
@@ -0,0 +1,13 @@
+# ZipMap
+
+ZipMap function constructs a map from a list of keys and a corresponding list of values.
+
+```sh
+$ tf console
+> zipmap(["pinaple", "oranges", "strawberry"],["yellow", "orange", "red"])
+{
+    "orange" = "orange"
+    "pineapple" = "yellow"
+    "strawberry" = "red"
+}
+```
